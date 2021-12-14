f2ddabdbfe5c1032199b793f39007535a8005575
Grant\GrantWiswell <GT101boy@gmail.com>
2021-12-14 11:12:30 -0600

    task implementation created

--- src/main/java/com/grantwiswell/batch_demo/MoveToArchive.java
+++ src/main/java/com/grantwiswell/batch_demo/MoveToArchive.java
@@ -5,9 +5,11 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class MoveToArchive implements Tasklet {
@@ -15,16 +17,37 @@ public class MoveToArchive implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 
+        System.out.println("\nMoveToArchive Start\n");
 
-        System.out.println("MoveToArchive Start");
 
-        try (Stream<Path> paths = Files.walk(Paths.get("C:/documents/"))) {
-            paths
-                    .filter(Files::isRegularFile)
-                    .forEach(Files::move);
+        File documentsDirectory = new File("./documents");
+        File archiveDirectory = null;
+
+        if (!documentsDirectory.isDirectory()) {
+            System.out.println("No documents directory");
+            return RepeatStatus.FINISHED;
         }
 
-        System.out.println("MoveToArchive Complete");
+        archiveDirectory = new File(documentsDirectory.getAbsolutePath()+"\\archive");
+
+        if (!archiveDirectory.exists()) {
+           archiveDirectory.mkdir();
+        }
+
+        File[] fileList = documentsDirectory.listFiles();
+
+        for (File file : fileList) {
+            if(!file.isFile())
+                continue;
+
+            String message = String.format("Moving %s to %s", file.getName(), archiveDirectory.getAbsolutePath());
+            System.out.println(message);
+
+            String newFilePath = String.format("%s\\%s", archiveDirectory.getAbsolutePath(), file.getName());
+            file.renameTo(new File(newFilePath));
+        }
+
+        System.out.println("\nMoveToArchive Complete");
         return RepeatStatus.FINISHED;
     }
 }
--- src/main/java/com/grantwiswell/batch_demo/PrintFileContents.java
+++ src/main/java/com/grantwiswell/batch_demo/PrintFileContents.java
@@ -5,24 +5,49 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class PrintFileContents implements Tasklet {
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        System.out.println("NotifyTask Start");
+        System.out.println("NotifyTask Start\n");
 
-        try (Stream<Path> paths = Files.walk(Paths.get("C:/"))) {
-            paths
-                    .filter(Files::isRegularFile)
-                    .forEach(System.out::println);
+        try {
+            File directoryPath = new File("./documents");
+
+            File[] fileList = directoryPath.listFiles();
+            Scanner scanner = null;
+            for (File file : fileList) {
+                if (!file.isFile())
+                    continue;
+
+                scanner = new Scanner(file);
+                String input;
+                StringBuffer stringBuffer = new StringBuffer();
+
+                while (scanner.hasNextLine()) {
+                    input = scanner.nextLine();
+                    stringBuffer.append(input).append("\n");
+                }
+
+                String message = String.format("Reading %s...\n%s", file.getAbsolutePath(), stringBuffer);
+                System.out.println(message);
+
+                scanner.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("PrintFileContents failed, file not found");
         }
 
-        System.out.println("NotifyTask Complete");
+        System.out.println("PrintFileContents Complete");
         return RepeatStatus.FINISHED;
     }
 }
